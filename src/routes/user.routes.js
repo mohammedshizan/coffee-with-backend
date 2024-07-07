5268e1c40bcd2a5d06844759a7b3551168b418d7
Mohammed shizan <you@example.com>
2024-07-07 10:45:24 +0530

    user registered

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,20 @@ import { registerUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
+    
 
 
 
